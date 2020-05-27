11d5b04718f6899b0471794390f04bf3e8c65cb6
Bart Evers <bart@gillz.nl>
2020-05-27 08:26:24 +0200

    Initialize data in constructor

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // tamagotchi.js
 class Tamagotchi {
+  constructor(name) {
+    this.name = name;
+    this.gender = "m";
+    this.family = "Zork";
+    this.birthDate = new Date();
 
+    this.hunger = 0;
+    this.happiness = 0;
+    this.discipline = 0;
+    this.age = 0;
+    this.numberOfPoops = 0;
+    this.weightGrams = 1000;
 
-  constructor() {
+    this.isIll = false;
     this.isHatched = false;
+    this.isPottyTrained = false;
 
+    this.lifeCycles = ["Baby", "Teen", "Adult", "Senior"];
+    this.currentLifeCycle = 0;
   }
   hatch() {
     console.log("Our egg is hatching! The miracle of life!");
     this.isHatched = true;
   }
+
 }
 
-const pet = new Tamagotchi();
+const pet = new Tamagotchi("Stinky");
 console.log(pet);
 pet.hatch();
 console.log(pet);
